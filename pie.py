625b5f64613c0fa4d294ec71610e2c7dde20b9af
coden stark <94005019+coden-s@users.noreply.github.com>
2024-01-16 15:44:33 -0800

    Update pie.py

--- pie.py
+++ pie.py
@@ -10,6 +10,7 @@ p3 = Pie("Ice Cream Pie", "Coockie and Cream Ice Cream", "Oreos")
 p4 = Pie("Broreo Pot Pie, Brussel Sprouts and Chicken", "Minced Oreo")
 p5 = Pie("Strawberry Rhubarb", "Strawberry and Ruhbarb Glaze", "graham crackers")
 p6 = Pie("Pumpkin", "pumpkin", "dough")
+p7 = Pie("Shepherd's Pie", "peas, carrots, and lamb", "mashed potatoes")
 
 
 print(p1.name)
