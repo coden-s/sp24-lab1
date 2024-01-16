604426e3f31025d6f3fc6232e2f06d518a5f16c7
Jordan Wirfs-Brock <jordanwb@gmail.com>
2024-01-15 21:45:52 -0800

    Update and rename person.py to pie.py

---
+++ pie.py
@@ -0,0 +1,11 @@
+class Pie:
+  def __init__(self, name, filling, crust):
+    self.name = name
+    self.filling = filling
+    self.crust = crust
+
+p1 = Pie("Key lime", "lime custard", "graham cracker")
+
+print(p1.name)
+print(p1.filling)
+print(p1.crust)
